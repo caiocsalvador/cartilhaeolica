eb789965f91d587eacacaca73ddb3e291db19e07
Caio César Salvador Santos <caiocsalvador@gmail.com>
2014-03-24 15:19:57 -0300

    Imagens e app rodando

--- js/action.js
+++ js/action.js
@@ -1,18 +1,20 @@
-alert('entrei');
+$().ready(function(){
 
-$('div.ui-page').on("swipeleft", function(event){
-	alert('1');
+	$('div.pagina').on("swipeleft", function(){
+		
 	var nextpage = $(this).next('div[data-role="page"]');
 	if (nextpage.length > 0) {
-	$.mobile.changePage(nextpage, "slide", false, true);
-}
-});
-
-$('div.ui-page').on("swiperight", function(event){
-	alert('2');
+	$.mobile.changePage(nextpage, {transition: "slide",
+		reverse: false} , false, true);
+	}
+	});
+	$('div.pagina').on("swiperight", function(){
+		
 	var prevpage = $(this).prev('div[data-role="page"]');
 	if (prevpage.length > 0) {
 	$.mobile.changePage(prevpage, {transition: "slide",
 	reverse: true}, true, true);
-}
+	}
+	});
+	
 });

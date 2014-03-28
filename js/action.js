ef1c521b89fb4ace5110b9adb29cbf053f45607e
Caio César Salvador Santos <caiocsalvador@gmail.com>
2014-03-28 15:13:05 -0300

    Paginas

--- js/action.js
+++ js/action.js
@@ -1,20 +1,21 @@
 $().ready(function(){
-
-	$('div.pagina').on("swipeleft", function(){
-		
-	var nextpage = $(this).next('div[data-role="page"]');
-	if (nextpage.length > 0) {
-	$.mobile.changePage(nextpage, {transition: "slide",
-		reverse: false} , false, true);
-	}
+	$('div.pagina').on("swipeleft", function(){		
+		var nextpage = $(this).next('div[data-role="page"]');
+		if (nextpage.length > 0) {	
+			if(!nextpage.children().length > 0){			
+				var pagina = ""+nextpage.attr("id")+".html";
+				nextpage.load(pagina);
+			}		
+		$.mobile.changePage(nextpage, {transition: "slide",
+			reverse: false} , false, true);
+		}
 	});
-	$('div.pagina').on("swiperight", function(){
-		
-	var prevpage = $(this).prev('div[data-role="page"]');
-	if (prevpage.length > 0) {
-	$.mobile.changePage(prevpage, {transition: "slide",
-	reverse: true}, true, true);
-	}
+	$('div.pagina').on("swiperight", function(){		
+		var prevpage = $(this).prev('div[data-role="page"]');
+		if (prevpage.length > 0) {
+		$.mobile.changePage(prevpage, {transition: "slide",
+		reverse: true}, true, true);
+		}
 	});
 	
 });

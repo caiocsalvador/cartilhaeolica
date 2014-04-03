80eb370f2b6791dbd91183cc44ef4d6e3b6ff467
Caio César Salvador Santos <caiocsalvador@gmail.com>
2014-04-03 17:23:49 -0300

    action

--- js/action.js
+++ js/action.js
@@ -1,4 +1,4 @@
-function onDeviceReady() {
+$().ready(function(){
 	$('div.pagina').on("swipeleft", function(){		
 		var nextpage = $(this).next('div[data-role="page"]');
 		if (nextpage.length > 0) {	
@@ -29,6 +29,6 @@ function onDeviceReady() {
 		var pagina = ""+nextpage.attr("id")+".html";		
 		nextpage.load(pagina);			
 	});
-}
-
-
+	
+	
+});
